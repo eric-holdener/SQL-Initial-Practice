f3c23a4be1a29fa2f0fefb34e59ed93aba9fb988
eric-holdener <48572094+eric-holdener@users.noreply.github.com>
2022-01-18 18:47:13 -0800

    Chapter 3 of Learning SQL
    exercises 3.1 - 3.4

---
+++ 3.1.sql
@@ -0,0 +1,3 @@
+select actor_id, first_name, last_name
+from actor
+order by last_name, first_name
---
+++ 3.2.sql
@@ -0,0 +1,3 @@
+select actor_id, first_name, last_name
+from actor
+where last_name = "Williams" or last_name = "Davis"
